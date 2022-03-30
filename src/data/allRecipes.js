ecd416d546fdcd82764b114cd81b0819d7a50b30
Julliet Nyaware <julienyaware@gmail.com>
2022-03-30 23:21:15 +0300

    Added file that combines all recipe objects into a single array of objects

---
+++ src/data/allRecipes.js
@@ -0,0 +1,28 @@
+import BananaOtmealCookie from './banana-oatmeal-cookie.json'
+import BasilAndPestoHummus from './basil-and-pesto-hummus.json'
+import BlackBeanAndRiceEnchiladas from './black-bean-and-rice-enchiladas.json'
+import DivineHardBoiledEggs from './divine-hard-boiled-eggs.json'
+import FourCheeseMargheritaPizza from './four-cheese-margherita-pizza.json'
+import HomemadeBlackBeanVeggie from './homemade-black-bean-veggie-burgers.json'
+import HomemadeChickenEnchiladas from './homemade-chicken-enchiladas.json'
+import MarinatedGrilledShrimp from './marinated-grilled-shrimp.json'
+import VegetableFriedRice from './vegetable-fried-rice.json'
+import VegeterianKorma from './vegetarian-korma.json'
+import WorldsBestLasagna from './worlds-best-lasagna.json'
+
+const allRecipes = []
+
+allRecipes.push(
+    BananaOtmealCookie,
+    BasilAndPestoHummus,
+    BlackBeanAndRiceEnchiladas,
+    DivineHardBoiledEggs,
+    FourCheeseMargheritaPizza,
+    HomemadeBlackBeanVeggie,
+    HomemadeChickenEnchiladas,
+    MarinatedGrilledShrimp,
+    VegetableFriedRice,
+    VegeterianKorma,
+    WorldsBestLasagna)
+
+export default allRecipes ;
